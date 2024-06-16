b6ff72ae513fbdc91d37df589b4eca4d947c510a
Lukas Potthast <privat@lukas-potthast.de>
2024-06-16 17:00:50 +0200

    Update testcontainers to v0.18

--- tests/keycloak_container/mod.rs
+++ tests/keycloak_container/mod.rs
@@ -1,5 +1,9 @@
 use keycloak::{KeycloakAdmin, KeycloakAdminToken, KeycloakTokenSupplier};
-use testcontainers::{core::WaitFor, runners::AsyncRunner, GenericImage, RunnableImage};
+use testcontainers::{
+    core::{ContainerPort, WaitFor},
+    runners::AsyncRunner,
+    GenericImage, ImageExt,
+};
 use url::Url;
 
 #[allow(dead_code)]
@@ -19,27 +23,27 @@ impl KeycloakContainer {
         let admin_user = "admin".to_owned();
         let admin_password = "admin".to_owned();
 
-        // This is equivalent to the following cli command:
+        // This setup is roughly equivalent to the following cli command:
         // `docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.0 start-dev`
-        let container: testcontainers::ContainerAsync<GenericImage> = RunnableImage::from(
-            GenericImage::new("quay.io/keycloak/keycloak", "25.0.0")
-                .with_env_var("KEYCLOAK_ADMIN", admin_user.as_str())
-                .with_env_var("KEYCLOAK_ADMIN_PASSWORD", admin_password.as_str())
-                .with_exposed_port(8080)
-                .with_wait_for(WaitFor::message_on_stdout(
-                    "Keycloak 25.0.0 on JVM (powered by Quarkus 3.8.5) started",
-                ))
-                .with_wait_for(WaitFor::message_on_stdout(
-                    "Listening on: http://0.0.0.0:8080",
-                ))
-                .with_wait_for(WaitFor::message_on_stdout(
-                    "Management interface listening on http://0.0.0.0:9000",
-                )),
-        )
-        .with_args(vec!["start-dev".to_owned()])
-        .start()
-        .await
-        .expect("Keycloak started");
+
+        let keycloak_image = GenericImage::new("quay.io/keycloak/keycloak", "25.0.0")
+            .with_exposed_port(ContainerPort::Tcp(8080))
+            .with_wait_for(WaitFor::message_on_stdout(
+                "Keycloak 25.0.0 on JVM (powered by Quarkus 3.8.5) started",
+            ))
+            .with_wait_for(WaitFor::message_on_stdout(
+                "Listening on: http://0.0.0.0:8080",
+            ))
+            .with_wait_for(WaitFor::message_on_stdout(
+                "Management interface listening on http://0.0.0.0:9000",
+            ));
+
+        let container_request = keycloak_image
+            .with_env_var("KEYCLOAK_ADMIN", admin_user.as_str())
+            .with_env_var("KEYCLOAK_ADMIN_PASSWORD", admin_password.as_str())
+            .with_cmd(["start-dev"]);
+
+        let container = container_request.start().await.expect("Keycloak started");
 
         let port = container
             .get_host_port_ipv4(8080)
